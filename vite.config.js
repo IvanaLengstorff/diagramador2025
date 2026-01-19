00035a18f3abc86109efb2361c44d23e24c678e1
MABO262001 <ivanalengstorff27@gmail.com>
2026-01-19 12:44:04 -0400

    cambios

--- vite.config.js
+++ vite.config.js
@@ -26,8 +26,8 @@ export default defineConfig({
         strictPort: false,
         hmr: {
             host: '98.83.42.61',
-            port: 5173,
-            protocol: 'http',
+            port: 443,
+            protocol: 'https',
         },
     },
 });
