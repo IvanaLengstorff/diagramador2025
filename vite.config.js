203d1197ade42b063b581b29eaded0ca284bf7d9
MABO262001 <ivanalengstorff27@gmail.com>
2026-01-19 12:20:24 -0400

    cambios

--- vite.config.js
+++ vite.config.js
@@ -19,23 +19,15 @@ export default defineConfig({
         outDir: 'public/build',
         emptyOutDir: true,
         manifest: true,
-        rollupOptions: {
-            output: {
-                manualChunks: {}
-            }
-        }
     },
     server: {
         host: '0.0.0.0',
         port: 5173,
         strictPort: false,
-        fs: {
-            strict: false,
-        },
         hmr: process.env.VITE_HMR_HOST ? {
             host: process.env.VITE_HMR_HOST,
-            port: 443,
-            protocol: 'https',
-        } : undefined,
+            port: process.env.VITE_HMR_PORT || 443,
+            protocol: process.env.VITE_HMR_PROTOCOL || 'https',
+        } : true,
     },
 });
